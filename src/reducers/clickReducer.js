22dbb706c66ce0799f5c8924bc2032bacaa128bc
Paulo Roberto <paulo.pereira.dev@gmail.com>
2020-09-24 09:55:14 -0300

    Action and actionTypes

--- src/reducers/clickReducer.js
+++ src/reducers/clickReducer.js
@@ -1,10 +1,12 @@
+import { CLICK_UPDATE_VALUE } from '../actions/actionTypes'
+
 const INITIAL_STATE = {
   newValue = ''
 }
 
 export const clickReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'CLICK_UPDATE_VALUE':
+    case CLICK_UPDATE_VALUE:
       return {
         ...state,
         newValue: action.newValue
