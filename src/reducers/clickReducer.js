90b4768f217cf12a960d775da38ecf47b9f2c2ae
Paulo Roberto <paulo.pereira.dev@gmail.com>
2020-09-24 09:48:38 -0300

    Create Reducer and combine than

---
+++ src/reducers/clickReducer.js
@@ -0,0 +1,15 @@
+const INITIAL_STATE = {
+  newValue = ''
+}
+
+export const clickReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'CLICK_UPDATE_VALUE':
+      return {
+        ...state,
+        newValue: action.newValue
+      }
+    default:
+      return state;
+  }
+}
